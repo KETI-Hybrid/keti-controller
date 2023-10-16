f092da669258e6093e6c51274e764fcbe2ea022b
root <hth0919@gmail.com>
2023-10-16 13:14:40 +0900

    metric add

--- client/keticlient.go
+++ client/keticlient.go
@@ -29,7 +29,7 @@ func NewForConfig(c *rest.Config) (*ClientSet, error) {
 	configShallowCopy := *c
 	if configShallowCopy.RateLimiter == nil && configShallowCopy.QPS > 0 {
 		if configShallowCopy.Burst <= 0 {
-			return nil, fmt.Errorf("Burst is required to be greater than 0 when RateLimiter is not set and QPS is set to greater than 0")
+			return nil, fmt.Errorf("burst is required to be greater than 0 when ratelimiter is not set and qps is set to greater than 0")
 		}
 		configShallowCopy.RateLimiter = flowcontrol.NewTokenBucketRateLimiter(configShallowCopy.QPS, configShallowCopy.Burst)
 	}
