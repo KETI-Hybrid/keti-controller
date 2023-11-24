a2ac3c569be4dd3631fefe64ccc3c1fc9a79d86d
jinho-park <lucky112233335@gmail.com>
2023-11-24 16:19:10 +0900

    Update nodemetric_controller.go

--- controllers/level/nodemetric_controller.go
+++ controllers/level/nodemetric_controller.go
@@ -1,62 +1,210 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package level
+package worker
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"metric-collector/pkg/api/grpc"
+	"metric-collector/pkg/api/kubelet"
+	"metric-collector/pkg/crd"
+	"metric-collector/pkg/decode"
+	"metric-collector/pkg/storage"
 
-	levelv1 "github.com/KETI-Hybrid/keti-controller/apis/level/v1"
+	keticlient "github.com/KETI-Hybrid/keti-controller/client"
+	"github.com/prometheus/client_golang/prometheus"
+	rpc "google.golang.org/grpc"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 )
 
-// NodeMetricReconciler reconciles a NodeMetric object
-type NodeMetricReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+var prevNode storage.NodeMetricsPoint
+var prevPods []storage.PodMetricsPoint
+
+const (
+	Nano = 10e-9
+	Giga = 1024 * 1024 * 1024
+)
+
+type MetricWorker struct {
+	KETINodeRegistry *prometheus.Registry
+	KETIPodRegistry  *prometheus.Registry
+	KubeletClient    *kubelet.KubeletClient
+	KetiClient       *keticlient.ClientSet
+	NodeManager      *NodeCollector
+	PodManager       *PodCollector
 }
 
-//+kubebuilder:rbac:groups=level.hybrid.keti,resources=nodemetrics,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=level.hybrid.keti,resources=nodemetrics/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=level.hybrid.keti,resources=nodemetrics/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the NodeMetric object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
-func (r *NodeMetricReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-
-	return ctrl.Result{}, nil
+type CollectorManager struct {
+	un *grpc.UnimplementedMetricGRPCServer
 }
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *NodeMetricReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&levelv1.NodeMetric{}).
-		Complete(r)
+func Initmetrics(nodeName string) *MetricWorker {
+	fmt.Println("Initializing metrics...")
+
+	worker := &MetricWorker{
+		KETINodeRegistry: prometheus.NewRegistry(),
+		KETIPodRegistry:  prometheus.NewRegistry(),
+	}
+
+	//reg := prometheus.NewPedanticRegistry()
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		config, err = clientcmd.BuildConfigFromFlags("", "/root/keti/config")
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	worker.KetiClient, err = crd.NewClient()
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	node, err := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+	if err != nil {
+		klog.Errorf("Error: %v\n", err)
+	}
+
+	for _, addr := range node.Status.Addresses {
+		if addr.Type == "InternalIP" {
+			worker.KubeletClient = kubelet.NewKubeletClient(addr.Address, config.BearerToken)
+			break
+		}
+	}
+	worker.NodeManager = NewNodeManager(worker.KETINodeRegistry, clientset, worker.KubeletClient)
+	worker.PodManager = NewPodManager(worker.KETIPodRegistry, clientset, worker.KubeletClient)
+	return worker
+}
+
+type NodeCollector struct {
+	ClientSet     *kubernetes.Clientset
+	KubeletClient *kubelet.KubeletClient
+	nodeMetric    *grpc.NodeMetric
+}
+
+func NewNodeManager(reg prometheus.Registerer, clientset *kubernetes.Clientset, kubeletClient *kubelet.KubeletClient) *NodeCollector {
+	newNodeMetic := grpc.NodeMetric{}
+	return &NodeCollector{
+		KubeletClient: kubeletClient,
+		ClientSet:     clientset,
+		nodeMetric:    &newNodeMetic,
+	}
+}
+
+func (nc NodeCollector) Collect() {
+	nodeName := os.Getenv("NODE_NAME")
+	for {
+		node, err := nc.ClientSet.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+		if err != nil {
+			klog.Errorln(err)
+		}
+
+		totalCPUQuantity := node.Status.Capacity["cpu"]
+		totalCPU, _ := totalCPUQuantity.AsInt64()
+		totalMemoryQuantity := node.Status.Capacity["memory"]
+		totalMemory, _ := totalMemoryQuantity.AsInt64()
+		totalStorageQuantity := node.Status.Capacity["ephemeral-storage"]
+		totalStorage, _ := totalStorageQuantity.AsInt64()
+
+		collection, err := Scrap(nc.KubeletClient, node)
+		if err != nil {
+			klog.Errorln(err)
+		}
+
+		nodeCores, _ := collection.Metricsbatch.Node.CPUUsageNanoCores.AsInt64()
+		nodeMemory, _ := collection.Metricsbatch.Node.MemoryUsageBytes.AsInt64()
+		prevRx := collection.Metricsbatch.Node.PrevNetworkRxBytes
+		prevTx := collection.Metricsbatch.Node.PrevNetworkTxBytes
+		nodeNetworkRx, _ := collection.Metricsbatch.Node.NetworkRxBytes.AsInt64()
+		nodeNetworkRx = nodeNetworkRx - prevRx
+		nodeNetworkTx, _ := collection.Metricsbatch.Node.NetworkTxBytes.AsInt64()
+		nodeNetworkTx = nodeNetworkTx - prevTx
+		nodeStorage, _ := collection.Metricsbatch.Node.FsUsedBytes.AsInt64()
+
+		nodeCPUPercent := (float64(nodeCores) * Nano) / float64(totalCPU)
+		nodeMemoryPercent := float64(nodeMemory) / float64(totalMemory)
+		nodeStoragePercent := float64(nodeStorage) / float64(totalStorage)
+
+		fmt.Println("Node core usage : ", (float64(nodeCores)*Nano)/40)
+		fmt.Println("Node core capacity : ", totalCPU)
+
+		nc.nodeMetric.NodeName = nodeName
+		nc.nodeMetric.NodeCores = int64(nodeCores)
+		nc.nodeMetric.TotalCPU = totalCPU
+		nc.nodeMetric.NodeCPUPercent = float32(nodeCPUPercent)
+
+		nc.nodeMetric.NodeMemory = nodeMemory
+		nc.nodeMetric.TotalMemory = totalMemory
+		nc.nodeMetric.NodeMemoryPercent = float32(nodeMemoryPercent)
+
+		nc.nodeMetric.NodeStorage = nodeStorage
+		nc.nodeMetric.TotalStorage = totalStorage
+		nc.nodeMetric.NodeStoragePercent = float32(nodeStoragePercent)
+
+		nc.nodeMetric.NodeNetworkRx = nodeNetworkRx
+		nc.nodeMetric.NodeNetworkTx = nodeNetworkTx
+
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func (nc NodeCollector) GetNode(ctx context.Context, req *grpcapi.)
+
+func StartGRPCServer(ctx context.Context, wg *sync.WaitGroup) {
+	lis, err := net.Listen("tcp", ":9323")
+	if err != nil {
+		klog.Fatalf("failed to listen: %v", err)
+	}
+
+	metricServer := rpc.NewServer()
+	grpc.RegisterMetricGRPCServer(metricServer, &NodeCollector{})
+
+	go func() {
+		if err := metricServer.Serve(lis); err != nil {
+			klog.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	wg.Done()
+}
+
+func Scrap(kubelet_client *kubelet.KubeletClient, node *v1.Node) (*storage.Collection, error) {
+	metrics, err := CollectNode(kubelet_client, node)
+	if err != nil {
+		klog.Errorf("unable to fully scrape metrics from node %s: %v", node.Name, err)
+	}
+
+	var errs []error
+	res := &storage.Collection{
+		Metricsbatch: metrics,
+		ClusterName:  os.Getenv("CLUSTER_NAME"),
+	}
+	return res, utilerrors.NewAggregate(errs)
+}
+
+func CollectNode(kubelet_client *kubelet.KubeletClient, node *v1.Node) (*storage.MetricsBatch, error) {
+
+	summary, err := kubelet_client.GetSummary()
+	if err != nil {
+		return nil, fmt.Errorf("unable to fetch metrics from Kubelet %s (%s): %v", node.Name, node.Status.Addresses[0].Address, err)
+	}
+
+	return decode.DecodeNodeBatch(summary, prevNode)
 }
