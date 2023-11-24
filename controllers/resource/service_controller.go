c64543cc8ae914b47e2df7699eedab298e468c12
jinho-park <lucky112233335@gmail.com>
2023-11-24 16:21:35 +0900

    Update service_controller.go

--- controllers/resource/service_controller.go
+++ controllers/resource/service_controller.go
@@ -1,62 +1,81 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package resource
+package service_controller
 
 import (
 	"context"
+	"time"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	resourcev1 "github.com/KETI-Hybrid/keti-controller/apis/resource/v1"
+	keticlient "github.com/KETI-Hybrid/keti-controller/client"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog/v2"
 )
 
-// ServiceReconciler reconciles a Service object
-type ServiceReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+type HouseKeeper struct {
+	KetiClient *keticlient.ClientSet
+	KubeClient *kubernetes.Clientset
 }
 
-//+kubebuilder:rbac:groups=resource.hybrid.keti,resources=services,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=resource.hybrid.keti,resources=services/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=resource.hybrid.keti,resources=services/finalizers,verbs=update
+func NewHouseKeeper(keticlient *keticlient.ClientSet, kubeclient *kubernetes.Clientset) *HouseKeeper {
+	return &HouseKeeper{
+		KetiClient: keticlient,
+		KubeClient: kubeclient,
+	}
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Service object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
-func (r *ServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-
-	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *ServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&resourcev1.Service{}).
-		Complete(r)
+func (hk *HouseKeeper) NodeKeeping() {
+	for {
+		nodeMap := make(map[string]bool)
+		nodes, err := hk.KubeClient.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			klog.Errorln(err)
+		}
+		for _, node := range nodes.Items {
+			nodeMap[node.Name] = true
+		}
+		nodeMetrics, err := hk.KetiClient.LevelV1().NodeMetrics().List(metav1.ListOptions{})
+		if err != nil {
+			klog.Errorln(err)
+		}
+		for _, nodeMetric := range nodeMetrics.Items {
+			if !nodeMap[nodeMetric.Name] {
+				err := hk.KetiClient.LevelV1().NodeMetrics().Delete(nodeMetric.Name, &metav1.DeleteOptions{})
+				if err != nil {
+					klog.Errorln(err)
+				}
+			}
+		}
+		time.Sleep(time.Second * 10)
+	}
+}
+
+func (hk *HouseKeeper) PodKeeping() {
+	for {
+		podMap := make(map[string]bool)
+		pods, err := hk.KubeClient.CoreV1().Pods(corev1.NamespaceAll).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			klog.Errorln(err)
+		}
+		for _, pod := range pods.Items {
+			if pod.Namespace == "cdi" || pod.Namespace == "keti-controller-system" || pod.Namespace == "keti-system" || pod.Namespace == "kube-flannel" || pod.Namespace == "kube-node-lease" || pod.Namespace == "kube-public" || pod.Namespace == "kube-system" || pod.Namespace == "kubevirt" {
+				continue
+			} else {
+				podMap[pod.Name] = true
+			}
+		}
+		podMetrics, err := hk.KetiClient.LevelV1().PodMetrics().List(metav1.ListOptions{})
+		if err != nil {
+			klog.Errorln(err)
+		}
+		for _, podMetric := range podMetrics.Items {
+			if !podMap[podMetric.Name] {
+				err := hk.KetiClient.LevelV1().PodMetrics().Delete(podMetric.Name, &metav1.DeleteOptions{})
+				if err != nil {
+					klog.Errorln(err)
+				}
+			}
+		}
+		time.Sleep(time.Second * 10)
+	}
 }
