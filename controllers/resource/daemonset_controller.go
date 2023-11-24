88d86ffe1f92a691ba33d2735c9213389aaedcaf
jinho-park <lucky112233335@gmail.com>
2023-11-24 16:16:48 +0900

    Update daemonset_controller.go

--- controllers/resource/daemonset_controller.go
+++ controllers/resource/daemonset_controller.go
@@ -33,23 +33,22 @@ type DaemonsetReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=resource.hybrid.keti,resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=resource.hybrid.keti,resources=daemonsets/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=resource.hybrid.keti,resources=daemonsets/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Daemonset object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *DaemonsetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var err error
+	result := &ClientManager{}
+
+	result.KetiClient, err = crd.NewClient()
+	if err != nil {
+		klog.Errorln(err)
+	}
+	result.KubeClient, err = k8s.NewClient()
+	if err != nil {
+		klog.Errorln(err)
+	}
+
+	return result
 
 	return ctrl.Result{}, nil
 }
